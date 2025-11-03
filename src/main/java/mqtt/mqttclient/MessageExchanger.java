b9e60d02a87058e003811265eef9b0c4d6e0caa2
焦丹杨 <1154206436@qq.com>
2025-11-03 21:00:24 +0800

    adjust

--- src/main/java/mqtt/mqttclient/MessageExchanger.java
+++ src/main/java/mqtt/mqttclient/MessageExchanger.java
@@ -10,12 +10,14 @@ import java.util.concurrent.*;
 
 /**
  * 将消息中转到对应的 Listener处理
+ *
+ * @todo 如果多线程处理消息，会出现消息乱序，需要设置自定义逻辑，配置式的开启是否多线程
  */
 public class MessageExchanger {
     private final ThreadPoolExecutor threadPoolExecutor;
     private final Map<String,MessageListener> subMap;
     MessageExchanger(Map<String, MessageListener> subMap){
-        this.threadPoolExecutor =  new ThreadPoolExecutor(4, 4, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), r -> {
+        this.threadPoolExecutor =  new ThreadPoolExecutor(1, 1, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), r -> {
             Thread thread = new Thread(r);
             thread.setName("Message--Thread");
             return thread;
@@ -30,11 +32,7 @@ public class MessageExchanger {
     public void submit(Message message){
         threadPoolExecutor.submit(()->{
             String topic = message.getTopic();
-            subMap.forEach((k,v)->{
-                if(TopicUtil.isMatch(k,topic)){
-                    v.receiveMsg(message);
-                }
-            });
+            subMap.get(topic).receiveMsg(message);
         });
     }
 
